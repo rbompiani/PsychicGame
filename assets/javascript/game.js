e4420cc681f4f15b7acdd1a3bf64ef4ff2257b3f
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-04-21 16:59:21 -0700

    wrote javascript pseudocode & created html outline

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// declare variables
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var compLetter;
+var userGuess;
+
+// generate a random letter (lowercase)
+
+// if guesses left > 0,
+
+// give user a chance to guess
+
+// verify guess is a string/letter
+
+// convert guess to lowercase
+
+// if userGuess = compLetter, add 1 to wins, reset guessesLeft, reset guessed letters
+
+// else write letter to guessed letters, reduce guessesLeft
