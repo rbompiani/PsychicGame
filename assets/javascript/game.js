cf66a62c13acf8254859937c416aa5226c5cbbf1
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-04-21 17:33:15 -0700

    added start function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,66 @@
 // declare variables
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guessesLeft;
 var compLetter;
 var userGuess;
 
 // generate a random letter (lowercase)
+function random_character() {
+    var chars = "abcdefghijklmnopqurstuvwxyz";
+    return chars.substr( Math.floor(Math.random() * 26), 1);
+}
 
-// if guesses left > 0,
+// start game
+// set compLetter
+function startGame(){
+    guesesLeft = 9;
+    // set compLetter
+    compLetter=random_character();
+    console.log("computer letter is: "+compLetter);
+    //reset guessed letters here
+    document.getElementById("guessed").innerHTML = "";
+}
 
-// give user a chance to guess
 
-// verify guess is a string/letter
+    // give user a chance to guess
+    document.onkeyup = function(event) {
+        userGuess = event.key;
+        //reduce guessesLeft
+        guessesLeft--;
+        console.log(guessesLeft,userGuess)
 
-// convert guess to lowercase
+        // verify guess is a string/letter
+        if (typeof userGuess === "string"){
+            // convert guess to lowercase
+            userGuess.toLowerCase();
+            // if userGuess = compLetter, add 1 to wins, reset guessesLeft, reset guessed letters
+            if(userGuess === compLetter){
+                wins++;
+                document.getElementById("wins").innerHTML = wins;
+                startGame();
+            }
+        }
 
-// if userGuess = compLetter, add 1 to wins, reset guessesLeft, reset guessed letters
+        if (guessesLeft===0){
+            console.log("you lose!");
+            losses++;
+            document.getElementById("losses").innerHTML = losses;
+            startGame();
+        }
 
-// else write letter to guessed letters, reduce guessesLeft
+                //reset guessed letters here
+            // else write letter to guessed letters, reduce guessesLeft
+          //  } else{
+            //    document.getElementById("guessed").innerHTML = userGuess;
+           // }
+       // }
+      };
+
+
+
+
+
+
+
+
