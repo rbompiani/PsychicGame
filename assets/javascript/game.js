127f3fd1c05ebeee22c5fe53b667c1bd6db08e47
Rebecca Bompiani <rebeccabompiani@gmail.com>
2019-04-21 18:08:29 -0700

    fully functioning game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 // declare variables
 var wins = 0;
 var losses = 0;
-var guessesLeft;
+var guessesLeft = 9;
 var compLetter;
 var userGuess;
+var guessed = "";
 
 // generate a random letter (lowercase)
 function random_character() {
@@ -14,47 +15,53 @@ function random_character() {
 // start game
 // set compLetter
 function startGame(){
-    guesesLeft = 9;
+    //reset guesses
+    guessesLeft = 9;
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+
     // set compLetter
     compLetter=random_character();
     console.log("computer letter is: "+compLetter);
+
     //reset guessed letters here
+    guessed = "";
     document.getElementById("guessed").innerHTML = "";
 }
-
 
     // give user a chance to guess
     document.onkeyup = function(event) {
         userGuess = event.key;
-        //reduce guessesLeft
-        guessesLeft--;
-        console.log(guessesLeft,userGuess)
 
         // verify guess is a string/letter
         if (typeof userGuess === "string"){
+
+            //reduce guessesLeft
+            guessesLeft--;
+            document.getElementById("guessesLeft").innerHTML = guessesLeft;
+            console.log(guessesLeft,userGuess);
+
             // convert guess to lowercase
             userGuess.toLowerCase();
+
+            // add letter to guessed letters
+            guessed += userGuess+", ";
+            document.getElementById("guessed").innerHTML = guessed;
+
             // if userGuess = compLetter, add 1 to wins, reset guessesLeft, reset guessed letters
             if(userGuess === compLetter){
                 wins++;
                 document.getElementById("wins").innerHTML = wins;
+                alert("YOU WIN! My letter was " +compLetter);
                 startGame();
             }
         }
-
         if (guessesLeft===0){
             console.log("you lose!");
             losses++;
             document.getElementById("losses").innerHTML = losses;
+            alert("YOU LOSE! My letter was " +compLetter);
             startGame();
-        }
-
-                //reset guessed letters here
-            // else write letter to guessed letters, reduce guessesLeft
-          //  } else{
-            //    document.getElementById("guessed").innerHTML = userGuess;
-           // }
-       // }
+        } 
       };
 
 
